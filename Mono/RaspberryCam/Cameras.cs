34c65f611688c243ddb8e8b1bb87f8920e37c94f
Flechner Romain <1033822+rflechner@users.noreply.github.com>
2012-10-16 17:11:47 +0200

    Add fluent syntax for multiple cameras management

---
+++ Mono/RaspberryCam/Cameras.cs
@@ -0,0 +1,22 @@
+namespace RaspberryCam
+{
+    public class Cameras
+    {
+        private readonly CamManager camManager;
+
+        public Cameras(CamManager camManager)
+        {
+            this.camManager = camManager;
+        }
+
+        public static CamManagerBuilder DeclareDevice()
+        {
+            return new CamManagerBuilder(new CamManager());
+        }
+
+        public CamDriver Get(string name)
+        {
+            return camManager.Get(name);
+        }
+    }
+}
---
+++ Mono/RaspberryCam/Interop/RaspberryCamInterop.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RaspberryCam.Interop
+{
+    public class RaspberryCamInterop
+    {
+        [DllImport("RaspberryCam.so", EntryPoint = "TakePicture")]
+        public static extern PictureBuffer TakePicture(string device, uint width, uint height, uint jpegQuantity);
+    }
+}
