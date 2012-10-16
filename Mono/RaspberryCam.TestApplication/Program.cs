527cf1cbe86fc21d216c225ee1e4c9b3dd1bbcac
Romain Flechner <Romain Flechner@localhost>
2012-10-16 15:05:48 +0200

    Can take pictures with interop in C#

--- Mono/RaspberryCam.TestApplication/Program.cs
+++ Mono/RaspberryCam.TestApplication/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace RaspberryCam.TestApplication
@@ -12,8 +14,17 @@ namespace RaspberryCam.TestApplication
             var pictureBuffer = RaspberryCamInterop.TakePicture("/dev/video0", 640, 480, 100);
 
             Console.WriteLine("Picture Buffer length: {0}", pictureBuffer.Size);
-            Console.WriteLine("Buffer data length: {0}", pictureBuffer.Data.Length);
+            //Console.WriteLine("Buffer data length: {0}", pictureBuffer.Data.Length);
 
+            //var c = pictureBuffer.Data[pictureBuffer.Size - 1];
+
+            var data = new byte[pictureBuffer.Size];
+
+            Marshal.Copy(pictureBuffer.Data, data, 0, pictureBuffer.Size);
+
+            Console.WriteLine("Buffer data length: {0}", data.Length);
+
+            File.WriteAllBytes("test.jpg", data);
         }
     }
 }
--- Mono/RaspberryCam/RaspberryCamInterop.cs
+++ Mono/RaspberryCam/RaspberryCamInterop.cs
@@ -9,12 +9,40 @@ namespace RaspberryCam
     public struct PictureBuffer
     {
         public int Size;
-        public string Data;
+        public IntPtr Data;
     }
 
     public class RaspberryCamInterop
     {
         [DllImport("RaspberryCam.so", EntryPoint = "TakePicture")]
         public static extern PictureBuffer TakePicture(string device, uint width, uint height, uint jpegQuantity);
+    }
+
+    public sealed class PictureBufferMarshaler : ICustomMarshaler
+    {
+        public object MarshalNativeToManaged(IntPtr pNativeData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IntPtr MarshalManagedToNative(object ManagedObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CleanUpNativeData(IntPtr pNativeData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CleanUpManagedData(object ManagedObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetNativeDataSize()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
