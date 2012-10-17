e44e5213ecbb044354435d9a7b97bc39c1b3e6dc
Romain Flechner <Romain Flechner@localhost>
2012-10-17 18:25:16 +0200

    Begin to create a test for video streaming

--- Mono/RaspberryCam/Interop/RaspberryCamInterop.cs
+++ Mono/RaspberryCam/Interop/RaspberryCamInterop.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace RaspberryCam.Interop
 {
@@ -6,5 +7,12 @@ namespace RaspberryCam.Interop
     {
         [DllImport("RaspberryCam.so", EntryPoint = "TakePicture")]
         public static extern PictureBuffer TakePicture(string device, uint width, uint height, uint jpegQuantity);
+
+        [DllImport("RaspberryCam.so", EntryPoint = "DisplaySrc")]
+        public static extern void DisplaySrc(IntPtr src);
+
+        [DllImport("RaspberryCam.so", EntryPoint = "FakeOpen")]
+        public static extern IntPtr FakeOpen(string device, uint width, uint height);
+
     }
 }
